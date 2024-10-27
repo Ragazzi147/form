6b4769f53d4e8f1306b17e8d0a7e19e73125a477
Lucas <lucasax147@gmail.com>
2024-10-27 01:15:33 -0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,95 @@
+import React from 'react';
+const formFields = [
+  {
+    id: 'nome',
+    label: 'Nome',
+    type: 'text',
+  },
+  {
+    id: 'email',
+    label: 'Email',
+    type: 'email',
+  },
+  {
+    id: 'senha',
+    label: 'Senha',
+    type: 'password',
+  },
+  {
+    id: 'cep',
+    label: 'Cep',
+    type: 'text',
+  },
+  {
+    id: 'rua',
+    label: 'Rua',
+    type: 'text',
+  },
+  {
+    id: 'numero',
+    label: 'Numero',
+    type: 'text',
+  },
+  {
+    id: 'bairro',
+    label: 'Bairro',
+    type: 'text',
+  },
+  {
+    id: 'cidade',
+    label: 'Cidade',
+    type: 'text',
+  },
+  {
+    id: 'estado',
+    label: 'Estado',
+    type: 'text',
+  },
+];
+
+const App = () => {
+  const [mensagem, setMensagem] = React.useState('');
+  const [form, setForm] = React.useState(
+    formFields.reduce((acc, field) => {
+      return {
+        ...acc,
+        [field.id]: '',
+      };
+    }, {}),
+  );
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    fetch('https://ranekapi.origamid.dev/json/api/usuario', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      // form é o objeto com os dados do formulário
+      body: JSON.stringify(form),
+    }).then((response) => {
+      setMensagem(response);
+    });
+  }
+
+  function handleChange({ target }) {
+    const { id, value } = target;
+    console.log(id, value);
+    setForm({ ...form, [id]: value });
+  }
+
+  return (
+    <form onSubmit={handleSubmit}>
+      {formFields.map(({ id, label, type }) => (
+        <div key={id}>
+          <label htmlFor={id}>{label}</label>
+          <input type={type} id={id} value={form[id]} onChange={handleChange} />
+        </div>
+      ))}
+      {mensagem && mensagem.ok && <p>Formulário Enviado</p>}
+      <button>Enviar</button>
+    </form>
+  );
+};
+
+export default App;
